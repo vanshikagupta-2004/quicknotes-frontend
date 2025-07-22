aa638e6be2bef5a33390489b69ea06270d81f995
Vanshika Gupta <vanshikagupta503@gmail.com>
2025-07-23 01:28:26 +0530

    Move _redirects into public folder for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'public', // ✅ tells Vite to include static files like _redirects
 })
